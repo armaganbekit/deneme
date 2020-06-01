b8a395276db8851ea117cb4e4eb523ddd7c86186
Glitch (dust-holy-cafe) <none>
2020-06-01 17:10:02 +0000

    🏀🌱 Checkpoint ./events/ready.js:16534670/2640 ./events/message.js:16534670/164 ./komutlar/komutlar.js:16534670/676 ./util/Util.js:16534670/3750 ./util/eventLoader.js:16534670/279 ./ayarlar.json:16534670/960 ./package.json:16534670/8624 ./shydracode.js:16534670/4139

---
+++ events/ready.js
@@ -0,0 +1,39 @@
+// Bu Altyapı Tamamen Shydra Codeye Aittir.
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(
+    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Aktif, Komutlar yüklendi!`
+  );
+  console.log(
+    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${
+      client.user.username
+    } ismi ile giriş yapıldı!`
+  );
+  client.user.setStatus("online");
+  var oyun = [
+    "S",
+    "Sh",
+    "Shy",
+    "Shyd",
+    "Shydr",
+    "Shydra",
+    "Shydr",
+    "Shyd",
+    "Shy",
+    "Sh",
+    "S"
+  ];
+
+  setInterval(function() {
+    var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
+
+    client.user.setActivity(oyun[random], "");
+  }, 2 * 2500);
+};
+// Bu Altyapı Tamamen Shydra Codeye Aittir.
