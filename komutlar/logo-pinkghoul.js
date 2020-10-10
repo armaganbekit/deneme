64886fef03fef406acec80e366941865616dfd3b
Glitch (dust-holy-cafe) <none>
2020-10-10 12:12:20 +0000

    🎨🛥 Checkpoint ./komutlar/logo-pinkghoul.js:22488622/731 ./komutlar/logo-purple-dragon.js:22488622/734 ./komutlar/sabertooth.js:22488622/732 ./komutlar/logo-angrycat.js:22488622/730 ./komutlar/piratesskull.js:22488622/696 ./komutlar/logo-piratesskull.js:22488622/1429 ./komutlar/logo-twitch.js:22488622/728

---
+++ komutlar/logo-pinkghoul.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args) => {
+  const yazi = args.slice(0).join('+'); 
+
+  if(!yazi) return message.channel.send(`**Lütfen yazı yazınız.** `)
+  const linqo = `https://dynamic.brandcrowd.com/asset/logo/236a12ee-2b79-4b58-b9e4-5536f5e93db7/logo?v=4&text=${yazi}`
+  .replace(' ', '+')
+
+  
+  const embed = new Discord.MessageEmbed()
+  .setTitle("Logo")
+  .setColor("RANDOM")
+  .setImage(linqo)
+  .setFooter('Logo Oluşturuldu')
+  message.channel.send(embed)
+}
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [''],
+    permLevel: 0
+}
+
+exports.help = {
+    name: 'pinkghoul',
+    description: 'Yazdığınız yazıyı bannera çevirir.',
+    usage: 'alev <yazı>'
+}
