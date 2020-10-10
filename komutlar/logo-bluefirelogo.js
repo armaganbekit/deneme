7aa4aadd24d29ebb6bcb8ddd81cf728bf0020a30
Glitch (dust-holy-cafe) <none>
2020-10-10 11:23:38 +0000

    🐸🚉 Checkpoint ./komutlar/bluefirelogo.js:22488622/748 ./komutlar/logo-bluefirelogo.js:22488622/19

---
+++ komutlar/logo-bluefirelogo.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args) => {
+  const yazi = args.slice(0).join('+'); 
+
+  if(!yazi) return message.channel.send(`**Lütfen yazı yazınız.** `)
+  const linqo = `https://dynamic.brandcrowd.com/asset/logo/52891976-5ac9-4aff-9939-a272b563577b/logov=4&text=${yazi}v=4&text=${yazi}`
+  .replace(' ', '+')
+
+  
+  const embed = new Discord.MessageEmbed()
+  .setTitle("Logo")
+  .setColor("RANDOM")
+  .setImage(linqo)
+  .setFooter('Logo Oluşturuldu')
+  message.channel.send(embed)
+}
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [''],
+    permLevel: 0
+}
+
+exports.help = {
+    name: 'bluegamefire',
+    description: 'Yazdığınız yazıyı bannera çevirir.',
+    usage: 'alev <yazı>'
+}
