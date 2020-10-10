90e2ba7a85ee854d82405969241e22687fb46686
Glitch (dust-holy-cafe) <none>
2020-10-10 12:45:35 +0000

    🎮🏀 Checkpoint ./komutlar/öneri.js:22488622/1403 ./package.json:22488622/2 ./bluefire-v12.js:22488622/702 ./komutlar/prefix.js:22488622/1135

--- bluefire-v12.js
+++ bluefire-v12.js
@@ -218,7 +218,48 @@ client.on("messageUpdate", (oldMessage, newMessage) => {
 });
 
 
+////prefix
+client.on("message", async message => {
 
+  if (message.author.bot) return;
+
+  if (!message.guild) return;
+
+  let prefix = db.get(`prefix_${message.guild.id}`);
+
+  if (prefix === null) prefix = prefix;
+
+
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+
+  if (!message.member)
+
+    message.member = await message.guild.fetchMember(message);
+
+
+
+  const args = message.content
+
+    .slice(prefix.length)
+
+    .trim()
+
+    .split(/ +/g);
+
+  const cmd = args.shift().toLowerCase();
+
+  if (cmd.length === 0) return;
+  
+  let command = client.commands.get(cmd);
+
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+  if (command) command.run(client, message, args);
+
+});
 
 /////////////
 //OTOROL
