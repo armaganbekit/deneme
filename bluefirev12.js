14bc4f4452cbe5b84c5c132eb55637a963f0b034
Glitch (dust-holy-cafe) <none>
2020-10-10 12:23:21 +0000

    👓🐘 Checkpoint ./komutlar/otorol.js:22488622/3611 ./bluefirev12.js:22488622/570

--- bluefirev12.js
+++ bluefirev12.js
@@ -128,7 +128,27 @@ client.on('error', e => {
 ///////////KOMUTLAR///////////////////////
 
 
+//otorol
+client.on("guildMemberAdd", async member => {
+  let kanal1 = await db.fetch(`otorolkanal_${member.guild.id}`);
+  let rol1 = await db.fetch(`otorolrol_${member.guild.id}`);
 
+  let kanal = member.guild.channels.cache.get(kanal1);
+  let rol = member.guild.roles.cache.get(rol1);
+
+  if (!kanal) return;
+  if (!rol) return;
+
+  const embed = new Discord.MessageEmbed()
+
+    .setColor("BLACK")
+    .setDescription(
+      `Sunucuya Katılan **${member}** Adlı Kullanıcıya Başarıyla \`${rol.name}\` Rolü Verildi.`
+    );
+
+  kanal.send(embed);
+  member.roles.add(rol);
+});
 
 
 //////////////////////////////////////////
