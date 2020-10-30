fc99e24af2b0fa004aa8a2e5392346a176024e4e
Glitch (dust-holy-cafe) <none>
2020-10-30 08:50:10 +0000

    👹🚦 Checkpoint ./komutlar/sayaçayarla.js:24302507/232

--- komutlar/sayaçayarla.js
+++ komutlar/sayaçayarla.js
@@ -6,20 +6,20 @@ let prefix = await db.fetch(`prefix.${message.guild.id}`) || fynx.prefix
 let kanal = message.mentions.channels.first() 
 let sayı = args[1]
 let kalan = args[1] - message.guild.memberCount
-if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`  **Bu komutu kullanabilmek için** "\`Yönetici\`" **yetkisine sahip olmalısın.**`);
+if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(` **Bu komutu kullanabilmek için** "\`Yönetici\`" **yetkisine sahip olmalısın.**`);
  
- if(!kanal) return message.channel.send(`<a:by:752306236606906399>  **Lütfen Bir Kanal Belirt!** \n**__Örnek Kullanım__** : \`${prefix}sayaç-ayarla #kanal <Sayı>\``)
+ if(!kanal) return message.channel.send(` **Lütfen Bir Kanal Belirt!** \n**__Örnek Kullanım__** : \`${prefix}sayaç-ayarla #kanal <Sayı>\``)
   
- if(isNaN(args[1])) return message.channel.send(`<a:by:752306236606906399>   **Belirttiğin Sayı Çok Küçük Veya O Sayıya Zaten Ulaşmışsın!**\n**__Örnek Kullanım__** : \`${prefix}sayaç-ayarla #kanal <Sayı>\``)
+ if(isNaN(args[1])) return message.channel.send(`   **Belirttiğin Sayı Çok Küçük Veya O Sayıya Zaten Ulaşmışsın!**\n**__Örnek Kullanım__** : \`${prefix}sayaç-ayarla #kanal <Sayı>\``)
  
- if(message.guild.memberCount > args[1]) return message.channel.send(`<a:by:752306236606906399>  **Belirttiğin Sayı Çok Küçük Veya O Sayıya Zaten Ulaşmışsın!**\n**__Örnek Kullanım__** : \`${prefix}sayaç-ayarla #kanal <Sayı>\``)
+ if(message.guild.memberCount > args[1]) return message.channel.send(` **Belirttiğin Sayı Çok Küçük Veya O Sayıya Zaten Ulaşmışsın!**\n**__Örnek Kullanım__** : \`${prefix}sayaç-ayarla #kanal <Sayı>\``)
 
  
   message.channel.send(`╔▬▬▬▬▬▬▬▬4RM4G4N Sayaç▬▬▬▬▬▬▬▬▬
-║► <a:hg:752305081545916438> Sayaç Aktif Edildi.
-║► <a:hg:752305081545916438> **${args[1]}** Olarak Güncelledim! 
-║► <a:hg:752305081545916438> Sayaç Kanalını **${kanal}** Olarak Güncelledim! 
-║► <a:hg:752305081545916438> ${args[1]} Kişi Olmaya Son <a:alev:752289999642296370> **${kalan}** <a:alev:752289999642296370> Kişi Kaldı!
+║►  Sayaç Aktif Edildi.
+║►  **${args[1]}** Olarak Güncelledim! 
+║►  Sayaç Kanalını **${kanal}** Olarak Güncelledim! 
+║►  ${args[1]} Kişi Olmaya Son  **${kalan}**  Kişi Kaldı!
 ╚▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬`)
 
   
